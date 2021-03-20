74eed7bfdb52326dd015c654e18946e6a98c58a2
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-20 08:22:02 +0100

    izmjene

---
+++ ex04/src/index.js
@@ -0,0 +1,11 @@
+function sumFibonacci(num) {
+  
+}
+
+console.log(sumFibonacci(1));
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
+
+module.exports = sumFibonacci;
---
+++ ex08/src/index.js
@@ -0,0 +1,39 @@
+var contacts = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    number: "12365495208",
+    likes: ["C", "C++", "C#"],
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    number: "4568295761",
+    likes: ["Phyton", "Swift", "R"],
+  },
+  {
+    firstName: "Richard",
+    lastName: "Doe",
+    number: "6938257149",
+    likes: ["React", "Angular", "Vue"],
+  },
+  {
+    firstName: "Jane",
+    lastName: "Roe",
+    number: "unknown",
+    likes: ["JavaScript", "Node", "HTML & CSS"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  
+}
+
+console.log(lookUpProfile("John", "likes"));
+console.log(lookUpProfile("Jane", "lastName"));
+console.log(lookUpProfile("Richard", "likes"));
+console.log(lookUpProfile("Rob", "number"));
+console.log(lookUpProfile("Rob", "JavaScript"));
+console.log(lookUpProfile("John", "address"));
+
+module.exports = lookUpProfile;
